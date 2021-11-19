9be362aa95ab05acaf77a2b2558d6ddcc0b8567a
Aung Myo Khant <aungmyokhant2465@gmail.com>
2021-11-19 20:59:39 +0630

    add cypress e2e test

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function() {
-    // beforeEach(function() {
-    //     cy.visit('http://localhost:5000')
-    // })
-    it('front page can be opened', function() {
+    beforeEach(function() {
         cy.visit('http://localhost:5000')
+        cy.wait(1000)
+    })
+    it('front page can be opened', function() {
         cy.contains('ivysaur')
         cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    })
+    })  
     it('pokemon page can be navigated to', function() {
-        cy.visit('http://localhost:5000')
         cy.contains('ivysaur').click()
         cy.contains('chlorophyll')
     })
